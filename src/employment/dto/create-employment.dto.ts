32d37db7c60297d3c0ac14fdc6ad3bc68b0f3b3a
Junsung Choi <choiexe1@gmail.com>
2023-10-08 16:22:36 +0900

    refactor: IsNotBlank 데코레이터는 옵셔널 파라미터에만 필요함

--- src/employment/dto/create-employment.dto.ts
+++ src/employment/dto/create-employment.dto.ts
@@ -1,30 +1,24 @@
 import { IsAlphanumeric, IsNotEmpty, IsString } from 'class-validator';
-import { IsNotBlank } from 'src/common/IsNotBlank.decorator';
 
 export class CreateEmploymentDto {
   @IsString()
   @IsNotEmpty()
-  @IsNotBlank()
   company_id: string;
 
   @IsString()
   @IsNotEmpty()
-  @IsNotBlank()
   position: string;
 
   @IsAlphanumeric()
   @IsNotEmpty()
-  @IsNotBlank()
   reward: number;
 
   @IsString()
   @IsNotEmpty()
-  @IsNotBlank()
   summary: string;
 
   @IsString()
   @IsNotEmpty()
-  @IsNotBlank()
   required_skill: string;
 
   objectification() {
