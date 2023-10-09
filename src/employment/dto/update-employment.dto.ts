de24ebad16306df0c8d0f2bc78060199c3981b5f
Junsung Choi <choiexe1@gmail.com>
2023-10-09 15:34:21 +0900

    feat: 공식문서에 따라, 권장하는 플러그인으로 변경

--- src/employment/dto/update-employment.dto.ts
+++ src/employment/dto/update-employment.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType } from '@nestjs/mapped-types';
+import { OmitType } from '@nestjs/swagger';
 import { CreateEmploymentDto } from './create-employment.dto';
 
 export class UpdateEmploymentDto extends OmitType(CreateEmploymentDto, [
